d1f25e313616fdd8f30a54adfa71e163a1dde282
unknown <tlefloch@gamingcampus.local>
2024-03-08 10:51:48 +0100

    continu code function choice levels

--- Assets/Script/ButtonFollowVisual.cs
+++ Assets/Script/ButtonFollowVisual.cs
@@ -1,62 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class ButtonFollowVisual : MonoBehaviour
 {
+    [Header("Visual")]
     [SerializeField] private Transform _visualTarget;
-    [SerializeField] private Vector3 _localAxis;        // Axis where the button will move
+    [SerializeField] private Material _initialMaterial;
+    [SerializeField] private Material _selectedMaterial;
+
+    [Header("Button Animation")]
+    [SerializeField] private Vector3 _localAxis;                    // Axis where the button will move
     [SerializeField] private float _speedMovement = 5;
-    [SerializeField] private float _freezeTime = 5;
     [SerializeField] private float _lenghtPress = 0.1f;
-    [SerializeField] private InputAction _inputPress;    // Input to press the button
+
+    [Header("Inputs")]
+    [SerializeField] private InputActionReference _leftHandPress;
+    [SerializeField] private InputActionReference _rightHandPress;
+
+    private bool _leftIsPressing = false;
+    private bool _rightIsPressing = false;
 
     private Vector3 _initialLocalPos;
+    private Vector3 _pressLocalPos;
 
     private XRBaseInteractable _interactable;
 
-    private bool _freeze = false;                       // Freeze the button when it's at the selected position
-    private bool _isPressing = false;
+    private bool _isPressing = false;                   // Button is pressed
+    private bool _isDown = false;                       // Button is down
+    private bool _isUp = false;                         // Button is up
+
+    void OnEnable()
+    {
+        _leftHandPress.action.started += ctx => _leftIsPressing = true;
+        _leftHandPress.action.canceled += ctx => _leftIsPressing = false;
+        _leftHandPress.action.Enable();
+
+        _rightHandPress.action.started += ctx => _rightIsPressing = true;
+        _rightHandPress.action.canceled += ctx => _rightIsPressing = false;
+        _rightHandPress.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        _leftHandPress.action.started -= ctx => _leftIsPressing = true;
+        _leftHandPress.action.canceled -= ctx => _leftIsPressing = false;
+        _leftHandPress.action.Disable();
+
+        _rightHandPress.action.started -= ctx => _rightIsPressing = true;
+        _rightHandPress.action.canceled -= ctx => _rightIsPressing = false;
+        _rightHandPress.action.Disable();
+    }
 
     void Start()
     {
         _initialLocalPos = _visualTarget.localPosition;
+        _pressLocalPos = _initialLocalPos + _localAxis * _lenghtPress;
 
         _interactable = GetComponentInParent<XRBaseInteractable>();
-        _interactable.hoverEntered.AddListener(Follow);
-        //_interactable.hoverExited.AddListener(Reset);
-        _interactable.selectEntered.AddListener(Freeze);
-
-        //_inputPress.Enable();
+        _interactable.hoverEntered.AddListener(PressButton);
     }
 
-    public void Follow(BaseInteractionEventArgs hover)
-    {
-        if (hover.interactorObject is XRRayInteractor/* && !_isPressing*/)
-        {
-            //_isPressing = true;
-            _freeze = false;
-            StartCoroutine(MoveButton());
-        }
-    }
-
-    public void Reset(BaseInteractionEventArgs hover)
+    public void PressButton(BaseInteractionEventArgs hover)
     {
         if (hover.interactorObject is XRRayInteractor)
         {
-            _isPressing = false;
-            _freeze = false;
-        }
-    }
-
-    public void Freeze(BaseInteractionEventArgs hover)
-    {
-        if (hover.interactorObject is XRRayInteractor)
-        {
-            _freeze = true;
+            // Si rayon Droite/Gauche touche et bouton Droite/Gauche appuyé
+            _isPressing = true;
+            _isDown = true;
         }
     }
 
@@ -64,56 +75,40 @@ public class ButtonFollowVisual : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_freeze)
-            return;
-
         if (_isPressing)
         {
-            // Move the button to the pressed position with an animation
-            _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _initialLocalPos + _localAxis * _lenghtPress, Time.deltaTime * _speedMovement);
-        }
-        else
-        {
-            // Come back to initial position with an animation
-            _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _initialLocalPos, Time.deltaTime * _speedMovement);
+            if (_isDown)// Move the button to the pressed position with an animation
+            {
+                _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _pressLocalPos, Time.deltaTime * _speedMovement);
+
+                if (_visualTarget.localPosition == _pressLocalPos)
+                {
+                    _isDown = false;
+                    _isUp = true;
+                }
+            }
+
+            if (_isUp)// Come back to initial position with an animation
+            {
+                _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _initialLocalPos, Time.deltaTime * _speedMovement);
+
+                if (_visualTarget.localPosition == _initialLocalPos)
+                {
+                    _isUp = false;
+                    _isPressing = false;
+                }
+            }
         }
     }
 
-    private IEnumerator MoveButton()
-    {
-        
-        _isPressing = true;
-        yield return new WaitForSeconds(_freezeTime);
-        _isPressing = false;
-    }
+    //StartCoroutine(MoveButton());
+    //private IEnumerator MoveButton()
+    //{
+
+    //    _isPressing = true;
+    //    _isDown = true;
+    //    // TO DO : Choice level
+    //    yield return new WaitForSeconds(_freezeTime);
+    //    _isPressing = false;
+    //}
 }
-
-
-
-//public class tmp : MonoBehaviour
-//{
-
-//    [SerializeField] InputActionReference _input;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        _input.action.started += Action_started;
-
-//    }
-
-//    private void OnDestroy()
-//    {
-//        _input.action.started -= Action_started;
-//    }
-
-//    private void Action_started(InputAction.CallbackContext obj)
-//    {
-//        throw new System.NotImplementedException();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//    }
-//}
