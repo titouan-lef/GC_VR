8c95e306d7018c93e9bbbb792b999c082aaf5b26
FrCollart <fcollart@gaming.tech>
2024-03-08 14:30:20 +0100

    [MERGE] Targets into Map

---
+++ Assets/Script/Game/LevelSetup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Void_Channel", menuName = "Scriptable Objects/LevelSetup")]
+public class LevelSetup : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
