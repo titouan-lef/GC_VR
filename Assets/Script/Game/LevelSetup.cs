6861476f2f544e2a0be1bf1d07b4ac9208db5d2a
FrCollart <fcollart@gaming.tech>
2024-03-11 14:36:27 +0100

    [ADD] Setup Level ScOb

--- Assets/Script/Game/LevelSetup.cs
+++ Assets/Script/Game/LevelSetup.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Void_Channel", menuName = "Scriptable Objects/LevelSetup")]
+[CreateAssetMenu(fileName = "LevelSetup", menuName = "Scriptable Objects/LevelSetup")]
 public class LevelSetup : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    List<Transform> targetPoses;
+
+    public List<Transform> GetTagetPoses()
     {
-        
+        return targetPoses;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetTargetPoses()
     {
-        
+        GameObject[] allTargets = GameObject.FindGameObjectsWithTag("Target");
+        foreach (GameObject target in allTargets)
+        {
+            targetPoses.Add(target.transform);
+        }
     }
 }
