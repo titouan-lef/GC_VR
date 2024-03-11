50b3ab49d812f68368f83b4af27259aa86dfb0d4
unknown <tlefloch@gamingcampus.local>
2024-03-11 17:57:32 +0100

    Continue menu

--- Assets/Script/Menu/MainMenu/MenuSelection.cs
+++ Assets/Script/Menu/MainMenu/MenuSelection.cs
@@ -1,66 +1,44 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class MenuSelection : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro _text;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private Color _initialColor;
+    [SerializeField] private Color _hoverColor;
+    [SerializeField] private string _sceneToLoad;
 
     private XRBaseInteractable _interactable;
 
-    //void Start()
-    //{
-    //    _interactable = GetComponent<XRBaseInteractable>();
-    //    _interactable.selectEntered.AddListener(PressButton);
-    //    _interactable.hoverEntered.AddListener(HoverEnterButton);
-    //    _interactable.hoverExited.AddListener(HoverExitButton);
-    //}
+    void Start()
+    {
+        _interactable = GetComponent<XRBaseInteractable>();
+        Debug.Log(_interactable);
+        _interactable.selectEntered.AddListener(MenuSelectionned);
+        _interactable.hoverEntered.AddListener(HoverEnterButton);
+        _interactable.hoverExited.AddListener(HoverExitButton);
 
-    //private void PressButton(BaseInteractionEventArgs hover)
-    //{
+        _text.color = _initialColor;
+    }
 
-    //    if (hover.interactorObject is XRRayInteractor)
-    //    {
-    //        _buttonPressEvent.Raise(_buttonPressIndex);
-    //        _isPressing = true;
-    //        _meshRenderer.material = _selectedMaterial;
-    //        _currentMaterial = _selectedMaterial;
-    //        StartCoroutine(MoveButton());
-    //    }
-    //}
+    private void MenuSelectionned(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is XRRayInteractor)
+        {
+            SceneManager.LoadScene(_sceneToLoad);
+        }
+    }
 
-    //private void HoverEnterButton(BaseInteractionEventArgs hover)
-    //{
-    //    _meshRenderer.material = _hoveredMaterial;
-    //}
+    private void HoverEnterButton(BaseInteractionEventArgs hover)
+    {
+        Debug.Log("HoverEnterButton");
+        _text.color = _hoverColor;
+    }
 
-    //private void HoverExitButton(BaseInteractionEventArgs hover)
-    //{
-    //    _meshRenderer.material = _currentMaterial;
-    //}
-
-    //private IEnumerator MoveButton()
-    //{
-    //    while (_visualTarget.localPosition != _pressLocalPos)
-    //    {
-    //        _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _pressLocalPos, Time.deltaTime * _speedMovement);
-    //        yield return null;
-    //    }
-
-    //    while (_visualTarget.localPosition != _initialLocalPos)
-    //    {
-    //        _visualTarget.localPosition = Vector3.Lerp(_visualTarget.localPosition, _initialLocalPos, Time.deltaTime * _speedMovement);
-    //        yield return null;
-    //    }
-
-    //    _isPressing = false;
-    //}
-
-    //public void Deselect()
-    //{
-    //    Debug.Log("Deselect");
-    //    _meshRenderer.material = _initialMaterial;
-    //    _currentMaterial = _initialMaterial;
-    //}
+    private void HoverExitButton(BaseInteractionEventArgs hover)
+    {
+       _text.color = _initialColor;
+    }
 }
